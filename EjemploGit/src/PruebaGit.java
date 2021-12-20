c1139d5461f4d09cb2899878a39691502e27948d
LulMg <96081042+LulMg@users.noreply.github.com>
2021-12-20 20:12:32 +0100

    Update PruebaGit.java

--- EjemploGit/src/PruebaGit.java
+++ EjemploGit/src/PruebaGit.java
@@ -4,6 +4,7 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Ejemplo git");
+			System.out.println("Versiónremota");
 	}
 
 }
